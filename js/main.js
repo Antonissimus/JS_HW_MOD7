68125ced1fb0c868cfb207b20634b9e0ab9a2509
Anton Kirillov <anton.kirillov@gmail.com>
2017-11-19 23:47:31 +0200

    complite step 2

--- js/main.js
+++ js/main.js
@@ -11,27 +11,48 @@
 //       образом удаляя input и button.
 const addInputGroup = function () {
     let panel = document.querySelector('.panel');
+    let resultArea = document.querySelector('.resultText');
     let inputGroup = document.createElement('div');
     let input = document.createElement('input');
     let button = document.createElement('button');
+
     inputGroup.classList.add('input-group');
     input.type = 'text';
     button.classList = ('button minus');
     button.innerHTML = '-';
-    panel.appendChild(inputGroup);
+    panel.insertBefore(inputGroup, resultArea);
     inputGroup.appendChild(input);
     inputGroup.appendChild(button);
     let minusButtons = document.querySelectorAll('.button.minus');
-    minusButtons.forEach(function(minusButton) {
+    minusButtons.forEach(function (minusButton) {
         minusButton.addEventListener('click', delInputGroup);
     })
 }
 
-const delInputGroup = function() {
-    let parentNode = this.parentNode;    
+const delInputGroup = function () {
+    let parentNode = this.parentNode;
     parentNode.remove();
-} 
+}
 
 let plusButton = document.querySelector('.button.plus');
 plusButton.addEventListener('click', addInputGroup);
 
+// Шаг #2
+//     - Внизу панели расположите textarea и кнопку «собрать», по нажатию которой текст из 
+//       элементов input будет выведен в textarea.
+//     - Текст из разных инпутов должен быть разделен строчной конструкцией --&&--.
+
+const collectInputs = function () {
+    let inputsValue = document.querySelectorAll('.panel input');
+    let textArea = document.querySelector('textarea');
+
+    let inputs = []
+    for (let i = 0; i < inputsValue.length; i++) {
+        inputs[i] = inputsValue[i].value
+    }
+    let result = inputs.join("--&&--");
+    textArea.innerHTML = result;
+}
+
+let collectButton = document.querySelector('.button.collect');
+collectButton.addEventListener('click', collectInputs);
